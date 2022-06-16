8d4e2abffb8b7ee75c8f2edea882d88491e71f9b
tarzerk <its.krei@gmail.com>
2022-06-16 08:49:06 -0500

    done

---
+++ android/app/src/main/kotlin/com/example/greengo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.greengo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/statistics_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StatisticsPage extends StatefulWidget {
+  const StatisticsPage({super.key});
+
+  @override
+  State<StatisticsPage> createState() => _StatisticsPageState();
+}
+
+class _StatisticsPageState extends State<StatisticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('data')),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
